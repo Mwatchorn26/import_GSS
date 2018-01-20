ec33679d3204e89c2e29ed4ae5062e0aa4caa2bd
Mwatchorn26 <mwatchorn26@gmail.com>
2018-01-20 15:02:07 -0500

    rejigged calling the init function

--- gss.py
+++ gss.py
@@ -4,10 +4,10 @@ import time
 import pandas as pd
 #from tabulate import tabulate
 import json
-import initialize
 import pudb
+import initialize
 
-
+conn_string=''
 
 #RENAME THE DOUBLE OCCURANCES OF 
 #update "res_partner"
@@ -23,68 +23,74 @@ import pudb
 #UPDATE res_partner set "country_id"=(select id from res_country where "name"='Canada') WHERE "state_id" IN (select "id" from res_country_state where country_id = (select id from res_country where "name"='Canada'));
 
 
-def RunMyQuery(cur, sqlQuery):
-    cur.execute(sqlQuery)
+def psyQuery(sqlQuery):
+
+    if conn_string=='':
+        with open('config.json') as f:
+            conf = json.load(f)
+            conn_string = "dbname={} user={} password={}".format(conf['database'], conf['user'], conf['passw'])
+
+    pudb.set_trace()
+    initialize.init_db.init_pg(conn_string)
+
+    try:
+        conn = psycopg2.connect(conn_string)
+     
+        # create a cursor
+        cur = conn.cursor()
+        
+        # execute the create tables queries
+        cur.execute(sqlQuery)
+ 
+        # close the communication with the PostgreSQL
+        cur.close()
+        conn.commit()
+        for notice in conn.notices:
+            print(notice)
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print()
+        print("ERROR")
+        print(error)
+        print()
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+            
+
+
     return
-
-
-
 
 #Ensure you have this json in a file called config.json in the same directory (I only use databse, user and passw).
 #{
 #  "database": "db_name",
-#  "schema": "schema_name",
 #  "user": "user_name",
-#  "host": "host_url",
-#  "port": "port_num",
 #  "passw": "user_password"
 #}
-
-
-
 
 def run():
     with open('config.json') as f:
         conf = json.load(f)
     pudb.set_trace()
     conn_string = "dbname={} user={} password={}".format(conf['database'], conf['user'], conf['passw'])
-    #conn_string = "dbname=fix_me user=fix_me password=fix_me"
 
-    #if conn_string == "dbname=fix_me user=fix_me password=fix_me":
-    #    print("OOPS: You forgot to set the DB name, DB user and DB password")
-    #    return
+    initialize.init_db(conn_string)
 
     try:
         conn = psycopg2.connect(conn_string)
-   
      
         # create a cursor
         cur = conn.cursor()
         
         # execute the create tables queries
         cur.execute(initialize.strInsertNewCategories)
-
-        #initialize(cur)
-
-
-
-    
-        # execute a statement
-        #print('PostgreSQL database version:')
-        #cur.execute('SELECT version()')
-        #BOM_TEXT = pd.read_sql('SELECT * from gss_migration_status WHERE gss_done', conn)
-	#print tabulate(BOM_TEXT, headers='keys', tablefmt='psql')
-        
-        # display the PostgreSQL database server version
-        #db_version = cur.fetchone()
-        #print(db_version)
  
-         # close the communication with the PostgreSQL
+        # close the communication with the PostgreSQL
         cur.close()
         conn.commit()
         for notice in conn.notices:
             print(notice)
-
 
     except (Exception, psycopg2.DatabaseError) as error:
         print()
--- initialize/init_db.py
+++ initialize/init_db.py
@@ -1,17 +1,21 @@
 import cust_to_lead
 import uom
 import gss_tables
-from .. import psyQuery as qry
+from gss import psyQuery as qry
 import time
 
-def init_pg:
+import pandas as pd
+
+def init_pg(config):
+    import pudb; pudb.set_trace()
     qry.sqlQuery(gss_tables.create_gss_tables)
 
     #wait for data to be copied over to the new tables...
-    x=1000
-    while x<1
-        sleep 10
-        x=x+1
+
+    gss_status={}
+    while not gss_status:
+        sleep(100)
+        gss_status = UpdatePgDbStatus(config)
 
     qry.sqlQuery(cust_to_lead.strInsertIntoLeads)
     qry.sqlQuery(cust_to_lead.strCopyOverTags)
@@ -20,3 +24,37 @@ def init_pg:
 
     qry.sqlQuery(uom.strInsertNewCategories)
     qry.sqlQuery(uom.strInsertNewUoM)
+
+def UpdatePgDbStatus(config):
+    conn_string = "dbname={} user={} password={}".format(conf['database'], conf['user'], conf['passw'])
+
+    try:
+        conn = psycopg2.connect(conn_string)
+
+        # create a cursor
+        cur = conn.cursor()
+
+        # execute the create tables queries
+        cur.execute(initialize.strInsertNewCategories)
+
+        df = pd.read_sql('select * from gss_migration_status where ', con=conn_string)
+   
+        pudb.set_trace() 
+
+        # close the communication with the PostgreSQL
+        cur.close()
+        conn.commit()
+        for notice in conn.notices:
+            print(notice)
+        return df
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print()
+        print("ERROR")
+        print(error)
+        print()
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
