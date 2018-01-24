b20b3283017c3cdc1b9772f8f3c24027a6e2d2f3
Mwatchorn26 <mwatchorn26@gmail.com>
2018-01-23 22:10:04 -0500

    Removed 'SUFFIX' AND 'JOB' so few AA's are generated. The suffixes will be used to create Design/Make/Test Tasks.

--- project/AA.py
+++ project/AA.py
@@ -1,7 +1,7 @@
 insertAnalyticAccounts="""insert into "account_analytic_account" ("name",code,company_id,description,parent_id,state,"type",
     use_tasks, use_issues, user_id, write_uid,use_timesheets,date_start, write_date, create_date)
     select "odooName" as "name", "odooCode" as code,1 as company_id,
-    upper(trim(GSS1."PROJECT")) || ' ' || upper(trim(GSS1."PRJ_DESCR")) || E'\n' || upper(trim(GSS1."JOB")) ||    '-' || upper(trim(GSS1."SUFFIX")) || ' ' || initcap(trim(GSS1."PHASE_DESCR")) || E'\n' || initcap(trim(GSS1."PART"))
+    upper(trim(GSS1."PROJECT")) || ' ' || upper(trim(GSS1."PRJ_DESCR")) || E'\n' || 'Section:' || upper(trim(GSS1."PHASE")) || E'\n' || 'Description:' || initcap(trim(GSS1."PHASE_DESCR"))
     as description,
     case when length(trim(GSS1."JOB"))>0 then 
     		(select id from account_analytic_account where code= upper(trim(GSS1."PROJECT")) limit 1)
@@ -19,7 +19,7 @@ insertAnalyticAccounts="""insert into "account_analytic_account" ("name",code,co
     clock_timestamp() as write_date,
     clock_timestamp() as create_date
     from 
-    (select "PHASE_DESCR", "PROJECT", "PHASE", "SUFFIX", "JOB", "PRJ_DESCR", "DATE_CREATED", "PART", 
+    (select distinct "PHASE_DESCR", "PROJECT", "PHASE", "PRJ_DESCR", "DATE_CREATED", 
     case when length(trim("PHASE_DESCR")) > 0 then
                 trim(concat(upper(trim("PROJECT")) , '  § ' , initcap(trim("PHASE_DESCR"))))
     	else
@@ -32,3 +32,9 @@ insertAnalyticAccounts="""insert into "account_analytic_account" ("name",code,co
          end as "odooCode"
     from "gss_ANALYTIC_ACCOUNTS"    ) as GSS1
     ;"""
+
+
+
+junk="""
+    upper(trim(GSS1."PROJECT")) || ' ' || upper(trim(GSS1."PRJ_DESCR")) || E'\n' || upper(trim(GSS1."JOB")) ||    '-' || upper(trim(GSS1."SUFFIX")) || ' ' || initcap(trim(GSS1."PHASE_DESCR")) || E'\n' || initcap(trim(GSS1."PART"))
+"""
