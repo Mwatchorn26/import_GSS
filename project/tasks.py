01eb8bf8cd2cf5ae4fe78baa39d4093e9148c62b
Mwatchorn26 <mwatchorn26@gmail.com>
2018-01-26 23:33:46 -0500

    tasks now spread out among the various sub-projects as they are supposed to. May still need to do work on 00X tasks.

--- project/tasks.py
+++ project/tasks.py
@@ -1,11 +1,13 @@
 insertTasks="""
+/*TASKS:*/
 insert into project_task (create_date, "sequence", write_uid, create_uid, date_start, company_id, project_id, description, 
 						kanban_state, write_date, active, stage_id, "name", reviewer_id, notes)
 select clock_timestamp() as create_date, 10 as sequence, 1 as write_uid, 1 as create_uid, 
-job_hdr."DATE_START" as date_start, 1 as company_id, (select id from project_project where analytic_account_id =aa.parent_id) as project_id, job_hdr."DESCRIPTION" as description,
+job_hdr."DATE_START" as date_start, 1 as company_id, (select id from project_project where analytic_account_id =aaa.id) as project_id, job_hdr."DESCRIPTION" as description,
 'normal' as kanban_state, clock_timestamp() as write_date, true as active, (select id from project_task_type where "name"='Active') as stage_id,
 job_hdr."DESCRIPTION" as "name", 1 as reviewer_id, job_hdr."DESCRIPTION" as "notes"
-from (select distinct "AA", position(E'\\' in "AA") as "SUBACCOUNT", "PHASE_DESCR", "PROJECT", "PHASE", "PRJ_DESCR", "DATE_CREATED", "JOB", "SUFFIX",
+from (
+select distinct "AA", position(E'\\' in "AA") as "SUBACCOUNT", "PHASE_DESCR", "PROJECT", "PHASE", "PRJ_DESCR", "DATE_CREATED", "JOB", "SUFFIX",
     case when length(trim("PHASE_DESCR")) > 0 then
                 trim(concat(upper(trim("PROJECT")) , ' > ' , initcap(trim("PHASE_DESCR"))))
     	else
@@ -15,10 +17,12 @@ from (select distinct "AA", position(E'\\' in "AA") as "SUBACCOUNT", "PHASE_DESC
     			trim(concat(upper(trim("PROJECT")) , ' > ' , trim("PHASE")))
          Else
                 upper(trim("PROJECT")) 
-         end as "odooCode",
-         (select id from account_analytic_account where code= upper(trim("PROJECT")) limit 1) as parent_id
-    from "gss_ANALYTIC_ACCOUNTS" where trim("JOB")<>'' and position(E'\\' in "AA")>0 order by "PROJECT","PHASE","JOB") as aa
-left join "gss_V_JOB_HEADER" as job_hdr on job_hdr."JOB"=aa."JOB" and job_hdr."SUFFIX"=aa."SUFFIX" 
-where job_hdr."SUFFIX"<>'' and job_hdr."JOB"<>'';
+         end as "odooCode"    
+    from "gss_ANALYTIC_ACCOUNTS" where trim("JOB")<>'' and position(E'\\' in "AA")>0 ) as gaa
+left join (select "JOB", "SUFFIX", "DATE_START", "DESCRIPTION" from "gss_V_JOB_HEADER") as job_hdr on job_hdr."JOB"=gaa."JOB" and job_hdr."SUFFIX"=gaa."SUFFIX"
+left join (select id, code from account_analytic_account) as aaa on aaa.code = gaa."odooCode"
+where job_hdr."SUFFIX"<>'' and job_hdr."JOB"<>''
+;
+
 
 """
